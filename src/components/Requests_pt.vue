21374a3824950b5926a0e4b1d4d04675eae9a282
Erik Jishammar <erikjishammar@hotmail.com>
2026-02-01 22:00:12 +0100

    feat: initiate pt request form with data structure and validation

--- src/components/Requests_pt.vue
+++ src/components/Requests_pt.vue
@@ -1,13 +1,50 @@
 <template>
     <div class="container"> <!-- in case we want to style later on-->
-        <h1>
-        PT requests
-        </h1>
+      <form @submit.prevent="submitForm">
+
+        <div>
+        <label>Your email: 
+          <input type="text" value="email" v-model="email" required>
+        </label>
+        </div>
+
+        <div>
+        <label> Messege: 
+          <textarea name="msg" v-model="msg" placeholder="Enter a message to your PT of choice"></textarea>
+        
+        </label>
+        </div>
+      </form>  
     </div>
   </template>
 
 <script>
+import { firebaseURL } from '@/firebase/firebase_config';
 export default {
+  data(){
+    return{
+      email:this.email,
+      msg: this.msg,
+      ptId:this.$route.params.id
+    }
+  }, methods: {
+    submitForm(){ 
+      if(this.email.trim() === ''){
+        console.error('You need to enter your email!')
+        return;
+      } 
+     if(this.msg.trim() === ''){
+      console.error('You need to enter your message')
+     } 
+     const msgData = {
+        email: this.email,
+        msg: this.msg
+     }     
+
+      fetch()
+
+    }
+  }
     
 }
 
